3725cc411085d41e11bf3fd0aead0a9db71d3201
William Duyck <fuzzyfox0@gmail.com>
2014-07-30 23:41:48 +0100

    Update loops animation

--- asset/js/rocket-animation.js
+++ asset/js/rocket-animation.js
@@ -4,7 +4,7 @@
 function smokeGenerator( x, y ) {
   // jitter the input a little
   x = x + ( ( Math.random() * 100 ) - 50 );
-  y = y + ( ( Math.random() * 100 ) - 50 );
+  y = y + ( ( Math.random() * 200 ) - 50 );
 
   var size = Math.random() * 60;
   var fadeDuration = ( Math.random() * 1500 ) + 400;
@@ -34,61 +34,83 @@ Snap.load( 'asset/svg/rocket-scene.svg', function( file ) {
 
   snap.append( file );
 
-  // rocket into launch position
-  rocketLayer.animate( {
-    transform: 'T0 0'
-  }, 5000, mina.easeout, function() {
-    // tower fall away
-    launchTower.animate( {
-      transform: 'T-50 100 R-40 500 500 S0.5'
-    }, 1000 , mina.ease, function() {
-      launchTower = this.remove();
-    });
+  function resetAnimation(){
+    rocketLayer.transform( 'T-1000 0 R0 S1' );
+    launchTower.transform( 'T0 0 R0 S1' );
+    launchPad.transform( 'T0 0 R0 S1' );
+    rocket.transform( 'T0 0 R0 S1' );
+    bunting.animate({
+      transform: 'T0 0 R0 S1'
+    }, 700);
+    rocketLayer.animate({
+      transform: 'T-400 0 R0 S1'
+    }, 5000);
+  }
 
-    // perception of rocket moving away from ground
-    launchPad.animate({
-      transform: 'T-200 0 S0.7'
-    }, 2000 );
+  (function loop() {
+    // rocket into launch position
+    rocketLayer.animate( {
+      transform: 'T0 0'
+    }, 5000, mina.easeout, function() {
+      // rocket take off smoke
+      var smokeTakeOffInterval = setInterval( function() {
+        rocket.append( smokeGenerator( 350, 500 ) );
+      }, 10 );
+      setTimeout( function() {
+        // tower fall away
+        launchTower.animate( {
+          transform: 'T-50 100 R-40 500 500 S0.5'
+        }, 1000 , mina.ease, function() {
+          // launchTower = this.remove();
+        });
 
-    // rocket take off
-    var smokeTakeOffInterval = setInterval( function() {
-      rocket.append( smokeGenerator( 350, 500 ) );
-    }, 10 );
-    rocket.animate({
-      transform: 'T50 0 R60 S0.7'
-    }, 2000, mina.ease, function() {
+        // perception of rocket moving away from ground
+        launchPad.animate({
+          transform: 'T-200 0 S0.7'
+        }, 2000 );
 
-      // ground disappear
-      launchPad.animate({
-        transform: 'T-500 0 S0.2'
-      }, 500, function() {
-        launchPad = this.remove();
-        clearInterval( smokeTakeOffInterval );
-      });
+        // rocket lift off
+        rocket.animate({
+          transform: 'T50 0 R60 S0.7'
+        }, 2000, mina.ease, function() {
 
-      // rocket fly across screen
-      var smokeFlyByInterval = setInterval( function() {
-        rocket.append( smokeGenerator( 350, 550 ) );
-      }, 50 );
-      rocket.animate({
-        transform: 'T700 -500 R60 S0.2'
-      }, 2000, function() {
-        setTimeout( function() {
-          rocket.transform( 'T-500 -100 R90 S0.7' ).animate({
-            transform: 'T2000 -100 R90 S1.4'
-          }, 1000, function() {
-            clearInterval( smokeFlyByInterval );
-            rocket = this.remove();
+          // ground disappear
+          launchPad.animate({
+            transform: 'T-500 0 S0.2'
+          }, 500, function() {
+            // launchPad = this.remove();
+            clearInterval( smokeTakeOffInterval );
           });
-        }, 1200 );
-      });
 
-      // make ground fall away
-      bunting.animate({
-        transform: 'T0 50'
-      }, 2000, function() {
-        bunting = this.remove();
-      });
+          // rocket fly across screen
+          var smokeFlyByInterval = setInterval( function() {
+            rocket.append( smokeGenerator( 350, 500 ) );
+            rocket.append( smokeGenerator( 350, 600 ) );
+          }, 50 );
+          rocket.animate({
+            transform: 'T700 -550 R60 S0.2'
+          }, 2000, function() {
+            setTimeout( function() {
+              rocket.transform( 'T-500 -200 R90 S0.7' ).animate({
+                transform: 'T2000 -100 R90 S1.4'
+              }, 1000, function() {
+                clearInterval( smokeFlyByInterval );
+                // rocket = this.remove();
+
+                setTimeout( resetAnimation, 1000 );
+                setTimeout( loop, 10000 );
+              });
+            }, 1200 );
+          });
+
+          // make ground fall away
+          bunting.animate({
+            transform: 'T0 50'
+          }, 2000, function() {
+            // bunting = this.remove();
+          });
+        });
+      }, 1000 );
     });
-  });
+  }());
 });
